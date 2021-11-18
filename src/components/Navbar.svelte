3689fdfc5e5f642ff71fff4a572920eba9af19c4
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-19 00:23:36 +0300

    chore: Add desktop styles to navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
   import MobileMenu from "$components/Navbar/MobileMenu.svelte";
+  import { links } from "$lib/links";
   let isOpen = false;
 </script>
 
 <template>
   <nav
-    class="container w-full flex flex-row items-center space-x-4 py-2 border-b-gray-200 border-b-1"
+    class="container w-full flex flex-row items-center space-x-4 py-2 lg:py-6 border-b-gray-200 border-b-1"
   >
     <button
-      class="md:hidden flex items-center justify-center"
+      class="lg:hidden flex items-center justify-center"
       on:click={() => (isOpen = !isOpen)}
     >
       <svg
@@ -22,6 +23,17 @@
     </button>
     <MobileMenu bind:isOpen />
     <a class="font-bold text-3xl" href="/">sneakers</a>
+    <ul class="hidden lg:block space-x-4 !ml-12">
+      {#each links as { name, url }}
+        <li class="inline-block">
+          <a
+            href={url}
+            class="text-gray-500 hover:(border-b-2 border-b-gray-500) px-1 pb-1"
+            >{name}</a
+          >
+        </li>
+      {/each}
+    </ul>
     <!-- Pusher - ml-auto doesn't work -->
     <div class="flex-1" />
     <!-- Right section -->
@@ -29,6 +41,10 @@
       <!-- Cart icon -->
       <img src="/icon-cart.svg" alt="Cart" class="w-5 h-5" />
     </button>
-    <img src="/image-avatar.png" alt="User avatar" class="w-6 h-6" />
+    <img
+      src="/image-avatar.png"
+      alt="User avatar"
+      class="w-6 h-6 lg:(w-10 h-10 !ml-8)"
+    />
   </nav>
 </template>
