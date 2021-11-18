902bd011540b758be7a4f1f0466e666e14cd5645
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-18 20:54:34 +0300

    fix: Add close button

--- src/components/Navbar/MobileMenu.svelte
+++ src/components/Navbar/MobileMenu.svelte
@@ -24,21 +24,26 @@
     >
       <!-- Backdrop -->
     </div>
-    <ul
+    <div
       use:portal={"#nav-portal"}
       use:clickOutside={() => (isOpen = false)}
       transition:fly={{ duration: 400, x: -200 }}
-      class="fixed top-0 left-0 w-[55vw] px-4 py-4 z-20 bg-white h-screen shadow-2xl"
+      class="fixed top-0 left-0 w-[70vw] px-4 py-4 z-20 bg-white h-screen shadow-2xl"
     >
-      {#each links as { url, name }}
-        <li>
-          <a
-            href={url}
-            class="text-lg font-bold"
-            on:click={() => (isOpen = false)}>{name}</a
-          >
-        </li>
-      {/each}
-    </ul>
+      <button on:click={() => (isOpen = false)}>
+        <img class="w-4.5 h-4.5" src="/icon-close.svg" alt="Close menu" />
+      </button>
+      <ul class="mt-8 space-y-4">
+        {#each links as { url, name }}
+          <li>
+            <a
+              href={url}
+              class="text-lg font-bold"
+              on:click={() => (isOpen = false)}>{name}</a
+            >
+          </li>
+        {/each}
+      </ul>
+    </div>
   {/if}
 </template>
