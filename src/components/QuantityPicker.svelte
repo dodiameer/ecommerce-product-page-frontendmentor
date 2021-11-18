504069a76f71e5ec629427bd1833d4c96b6d6d95
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-19 01:01:12 +0300

    fix: Misaligned icons

--- src/components/QuantityPicker.svelte
+++ src/components/QuantityPicker.svelte
@@ -7,7 +7,7 @@
     class="rounded-md bg-true-gray-100 flex items-center justify-between px-4 py-3 shadow-inner"
   >
     <button
-      class="h-8 w-8"
+      class="h-8 w-8 flex items-center justify-center"
       on:click={() => {
         if (quantity !== 0) quantity -= 1;
       }}
@@ -16,7 +16,7 @@
     </button>
     <p class="font-bold">{quantity}</p>
     <button
-      class="h-8 w-8"
+      class="h-8 w-8 flex items-center justify-center"
       on:click={() => {
         quantity += 1;
       }}
