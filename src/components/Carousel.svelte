dc3d397cfe819fee44c393588a2e89cb5a2c0952
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-19 00:36:54 +0300

    chore: Add desktop styles to home page

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
   export let images: string[];
+  let userClass = "";
+  export { userClass as class };
   let currentImage = 0;
 
   const nextImage = () => {
@@ -14,37 +16,39 @@
 </script>
 
 <template>
-  <div hidden class="hidden">
-    <!-- Browser will load and cache them so they won't have to load when shown in carousel -->
-    {#each images as image}
-      <img src={image} alt="" />
-    {/each}
-  </div>
-  <section class="relative bg-primary-200">
-    <button
-      on:click={previousImage}
-      class="absolute top-1/2 left-2 z-10  bg-white rounded-full w-9 h-9 flex items-center justify-center"
-    >
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img src="/icon-previous.svg" alt="Previous image" class="w-2.5 h-4" />
-    </button>
-    <div class="flex flex-row items-center overflow-hidden max-h-82">
-      {#key currentImage}
-        <img
-          in:fade={{ duration: 400, delay: 400 }}
-          out:fade={{ duration: 300, delay: 0 }}
-          src={images[currentImage]}
-          alt=""
-          class="w-full h-auto object-contain"
-        />
-      {/key}
+  <div class={userClass}>
+    <div hidden class="hidden">
+      <!-- Browser will load and cache them so they won't have to load when shown in carousel -->
+      {#each images as image}
+        <img src={image} alt="" />
+      {/each}
     </div>
-    <button
-      on:click={nextImage}
-      class="absolute top-1/2 right-2 z-10 bg-white rounded-full w-9 h-9 flex items-center justify-center"
-    >
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img src="/icon-next.svg" alt="Next image" class="w-2.5 h-4" />
-    </button>
-  </section>
+    <section class="relative bg-primary-200">
+      <button
+        on:click={previousImage}
+        class="absolute top-1/2 left-2 z-10  bg-white rounded-full w-9 h-9 flex items-center justify-center"
+      >
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="/icon-previous.svg" alt="Previous image" class="w-2.5 h-4" />
+      </button>
+      <div class="flex flex-row items-center overflow-hidden max-h-82">
+        {#key currentImage}
+          <img
+            in:fade={{ duration: 400, delay: 400 }}
+            out:fade={{ duration: 300, delay: 0 }}
+            src={images[currentImage]}
+            alt=""
+            class="w-full h-auto object-contain"
+          />
+        {/key}
+      </div>
+      <button
+        on:click={nextImage}
+        class="absolute top-1/2 right-2 z-10 bg-white rounded-full w-9 h-9 flex items-center justify-center"
+      >
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src="/icon-next.svg" alt="Next image" class="w-2.5 h-4" />
+      </button>
+    </section>
+  </div>
 </template>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,24 +13,45 @@
 </script>
 
 <template>
-  <Carousel {images} />
-  <section class="container mt-4 pb-4">
-    <p class="text-primary text-sm uppercase font-bold">Sneaker Company</p>
-    <p class="text-3xl font-bold capitalize mt-1">
-      Fall limited edition sneakers
-    </p>
-    <p class=" text-gray-500 mt-4">
-      These low-profile sneakers are your perdect casual wear companion.
-      Featuring a durable rubber outer sold, they'll withstand everything the
-      weather can offer.
-    </p>
-    <div class="flex flex-row items-center mt-2 py-2">
-      <p class="text-xl font-bold">$125.00</p>
-      <Badge class="ml-2">50%</Badge>
-      <p class="ml-auto text-gray-500 text-sm"><s>$250.00</s></p>
+  <Carousel class="lg:hidden" {images} />
+  <section
+    class="container mt-4 pb-4 lg:(grid grid-cols-2 gap-4 place-items-center)"
+  >
+    <img
+      src={images[0]}
+      alt=""
+      class="<lg:hidden max-h-[calc(100vh-8rem)] rounded-lg"
+    />
+    <div>
+      <p class="text-primary text-sm uppercase font-bold">Sneaker Company</p>
+      <p class="text-3xl font-bold capitalize mt-1">
+        Fall limited edition sneakers
+      </p>
+      <p class=" text-gray-500 mt-4 max-w-[60ch]">
+        These low-profile sneakers are your perdect casual wear companion.
+        Featuring a durable rubber outer sold, they'll withstand everything the
+        weather can offer.
+      </p>
+      <div
+        class="flex flex-row items-center lg:(flex-col items-start) mt-2 py-2"
+      >
+        <div class="flex items-center">
+          <p class="text-xl lg:text-2xl font-bold">$125.00</p>
+          <Badge class="ml-2">50%</Badge>
+        </div>
+        <p class="<lg:ml-auto text-gray-500 font-bold text-sm">
+          <s>$250.00</s>
+        </p>
+      </div>
+      <div class="lg:(flex items-center space-x-4 mt-4)">
+        <div class="lg:w-1/3">
+          <QuantityPicker />
+        </div>
+        <div class="mb-2 lg:hidden" />
+        <div class="lg:w-2/3">
+          <AddToCartButton />
+        </div>
+      </div>
     </div>
-    <QuantityPicker />
-    <div class="mb-2" />
-    <AddToCartButton />
   </section>
 </template>
