e544a40f8b91b1a5d4037d6e73111164b78bfb9e
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-19 01:34:13 +0300

    feat: Add lightbox to mobile carousel

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
+  import Lightbox from "./Lightbox.svelte";
   export let images: string[];
   let userClass = "";
   export { userClass as class };
+
+  let isLightboxOpen = false;
   let currentImage = 0;
 
   const nextImage = () => {
@@ -11,8 +14,6 @@
   const previousImage = () => {
     currentImage = (currentImage - 1 + images.length) % images.length;
   };
-
-  $: console.log({ currentImage });
 </script>
 
 <template>
@@ -26,20 +27,22 @@
     <section class="relative bg-primary-200">
       <button
         on:click={previousImage}
-        class="absolute top-1/2 left-2 z-10  bg-white rounded-full w-9 h-9 flex items-center justify-center"
+        class="absolute top-1/2 left-2 z-10 bg-white rounded-full w-9 h-9 flex items-center justify-center"
       >
         <!-- svelte-ignore a11y-img-redundant-alt -->
         <img src="/icon-previous.svg" alt="Previous image" class="w-2.5 h-4" />
       </button>
       <div class="flex flex-row items-center overflow-hidden max-h-82">
         {#key currentImage}
-          <img
-            in:fade={{ duration: 400, delay: 400 }}
-            out:fade={{ duration: 300, delay: 0 }}
-            src={images[currentImage]}
-            alt=""
-            class="w-full h-auto object-contain"
-          />
+          <button on:click={() => (isLightboxOpen = true)}>
+            <img
+              in:fade={{ duration: 400, delay: 400 }}
+              out:fade={{ duration: 300, delay: 0 }}
+              src={images[currentImage]}
+              alt=""
+              class="w-full h-auto object-contain"
+            />
+          </button>
         {/key}
       </div>
       <button
@@ -51,4 +54,12 @@
       </button>
     </section>
   </div>
+  {#if isLightboxOpen}
+    <Lightbox
+      src={images[currentImage]}
+      alt=""
+      on:close={() => (isLightboxOpen = false)}
+      class="max-w-xl w-[calc(100vw-4rem)]"
+    />
+  {/if}
 </template>
